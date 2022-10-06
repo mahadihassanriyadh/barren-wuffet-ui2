b0f28a309a335dafaf17d511cd9a65c943146b51
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-10-06 20:41:17 +0600

    form updated

--- src/components/CreateFund/CreateFundForm.tsx
+++ src/components/CreateFund/CreateFundForm.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, SetStateAction, useState } from "react";
 import styles from "./CreateFundForm.module.css";
 import telegramIcon from "../../img/icons/telegramYellowIcon.svg"
 import twitterIcon from "../../img/icons/twitterYellowIcon.svg"
@@ -6,12 +6,49 @@ import discordIcon from "../../img/icons/discordYellowIcon.svg"
 import calendarIcon from "../../img/icons/calendarYellowIcon.svg"
 import parcentageIcon from "../../img/icons/parcentageYellowIcon.svg"
 import { NavLink } from "react-router-dom";
+import Error from "../ui/Error";
 
 const CreateFundForm: FunctionComponent = () => {
+  const [telegram, setTelegram] = useState('https://t.me/');
+  const [twitter, setTwitter] = useState('https://twitter.com/');
+  const [discord, setDiscord] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleTelegram = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setTelegram(e.target.value);
+  };
+  const handleTwitter = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setTwitter(e.target.value);
+  };
+  const handleDiscord = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setDiscord(e.target.value);
+  };
+  const handleFormSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    if (!telegram.startsWith('https://t.me/')) {
+      setIsError(true);
+      setError('Telegram input must starts with https://t.me/')
+      console.log(error)
+      return
+    }
+    if (!twitter.startsWith('https://twitter.com/')) {
+      setIsError(true);
+      setError('Twitter input must starts with https://twitter.com/')
+      console.log(error)
+      return
+    }
+    setIsError(false);
+    setError('');
+
+  }
   return (
     <div className="bg-[#1c1b25] mt-10 py-20 px-14 rounded-2xl shadow-xl">
       <div>
-        <form >
+        <form onSubmit={handleFormSubmit}>
+          {
+            isError && <Error error={error} />
+          }
           <p className="text-white">Information about your fund</p>
           <div className="mt-4 space-y-3">
             <input type="text" name="fundName" id="fundName" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-2.5" placeholder="Your Fund Name" required />
@@ -20,15 +57,15 @@ const CreateFundForm: FunctionComponent = () => {
             <div className="flex justify-between space-x-8">
               <div className="relative w-full">
                 <img className="absolute right-4 top-3" src={telegramIcon} alt="telegram icon" />
-                <input value={"https://t.me/username"} type="text" name="telegram" id="telegram" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Telegram" required />
+                <input onChange={handleTelegram} value={telegram} type="text" name="telegram" id="telegram" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Telegram" required />
               </div>
               <div className="relative w-full">
                 <img className="absolute right-4 top-3" src={twitterIcon} alt="telegram icon" />
-                <input type="text" name="twitter" id="twitter" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Twitter" required />
+                <input onChange={handleTwitter} value={twitter} type="text" name="twitter" id="twitter" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Twitter" required />
               </div>
               <div className="relative w-full">
                 <img className="absolute right-4 top-3" src={discordIcon} alt="telegram icon" />
-                <input type="text" name="discord" id="discord" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Discord" required />
+                <input onChange={handleDiscord} value={discord} type="text" name="discord" id="discord" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Discord Username" required />
               </div>
             </div>
           </div>
@@ -38,20 +75,20 @@ const CreateFundForm: FunctionComponent = () => {
             <div className="flex justify-between space-x-8">
               <div className="relative w-full">
                 <img className="absolute right-4 top-3" src={calendarIcon} alt="telegram icon" />
-                <input type="text" name="durationOfRaise" id="durationOfRaise" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Duration of raise" required />
+                <input type="date" name="durationOfRaise" id="durationOfRaise" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Duration of raise" required />
               </div>
               <div className="relative w-full">
                 <img className="absolute right-4 top-4" src={parcentageIcon} alt="telegram icon" />
-                <input type="text" name="fees" id="fees" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Fees" required />
+                <input type="number" name="fees" id="fees" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Fees" required />
               </div>
             </div>
           </div>
 
-          <NavLink className="hover:text-orange-400 flex justify-center mt-10" to="/fund/portfolio">
-              <button className="px-16 py-2.5 bg-gradient-to-r from-orange-600 to-orange-400 rounded-lg font-bold text-lg text-white">
+          <div className="hover:text-orange-400 flex justify-center mt-10">
+              <button type="submit" className="px-16 py-2.5 bg-gradient-to-r from-orange-600 to-orange-400 rounded-lg font-bold text-lg text-white">
                 Create Fund
               </button>
-          </NavLink>
+          </div>
         </form>
       </div>
     </div>
