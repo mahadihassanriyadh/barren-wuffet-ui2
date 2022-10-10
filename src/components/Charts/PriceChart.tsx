38d4350ce7d9655e16295c22f5aaa725009ae982
Mad Neutrino <madneutrino@jezer0x.com>
2022-10-10 16:09:34 +0800

    Add trading view chart in trading page with some basic settings and dummy dat

--- src/components/Charts/PriceChart.tsx
+++ src/components/Charts/PriceChart.tsx
@@ -1,24 +1,88 @@
-import React from "react";
+import React, { FunctionComponent, useEffect, useRef } from "react";
+import { createChart, LineStyle, CrosshairMode } from "lightweight-charts";
+import { generateCandlestickData } from "../../api/priceChart";
 
-export default function PriceChart(props: {
+const PriceChart: FunctionComponent<{
   title: string;
   fromToken: string;
   toToken: string;
-  priceFeed: () => { price: number; timestamp: number }[];
-}) {
-  const { title, priceFeed } = props;
-  const tokenPair = "USD/ETH";
-  const prices = priceFeed();
-  return (
-    <div>
-      <h2>{title}</h2>
-      <p>Token Pair: {tokenPair}</p>
-      {prices.map((p) => (
-        <div>
-          <p>Price: {p.price}</p>
-          <p>Time: {p.timestamp}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
+  priceFeed: () => { value: number; time: number }[];
+}> = (props) => {
+  const backgroundColor = "#222";
+  const lineColor = "#2962FF";
+  const textColor = "#C3BCDB";
+  const crossHairColor = "#C3BCDB44";
+  const gridColor = "#444";
+  const borderColor = "#71649C";
+  const chartContainerRef = useRef({} as HTMLDivElement);
+
+  useEffect(() => {
+    const handleResize = () => {
+      chart.applyOptions({ width: chartContainerRef.current.clientWidth });
+    };
+
+    const chart = createChart(chartContainerRef.current, {
+      layout: {
+        background: { color: backgroundColor },
+        textColor: textColor,
+      },
+      grid: {
+        vertLines: { color: gridColor },
+        horzLines: { color: gridColor },
+      },
+      width: chartContainerRef.current.clientWidth,
+      height: 300,
+      crosshair: {
+        // Change mode from default 'magnet' to 'normal'.
+        // Allows the crosshair to move freely without snapping to datapoints
+        mode: CrosshairMode.Normal,
+
+        // Vertical crosshair line (showing Date in Label)
+        vertLine: {
+          width: 4,
+          color: crossHairColor,
+          style: LineStyle.Solid,
+        },
+
+        // Horizontal crosshair line (showing Price in Label)
+        horzLine: {
+          color: crossHairColor,
+        },
+      },
+      localization: {
+        priceFormatter: (amt: number) => `${amt} eth`,
+      },
+    });
+    chart.priceScale().applyOptions({
+      borderColor: borderColor,
+    });
+    chart.timeScale().applyOptions({
+      borderColor: borderColor,
+    });
+    chart.timeScale().fitContent();
+
+    const newSeries = chart.addCandlestickSeries();
+    const candleStickData = generateCandlestickData();
+
+    newSeries.setData(candleStickData);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+
+      chart.remove();
+    };
+  }, [
+    backgroundColor,
+    lineColor,
+    textColor,
+    gridColor,
+    crossHairColor,
+    borderColor,
+  ]);
+
+  return <div ref={chartContainerRef} />;
+};
+
+export default PriceChart;
