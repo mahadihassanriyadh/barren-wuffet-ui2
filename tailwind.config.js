8ea40c802eddd64566c8d699895b201a8128aeb2
Mad Neutrino <madneutrino@jezer0x.com>
2022-10-19 15:53:54 +0800

    Fixed Tailwind config. The syntax wasnt quite right, so it wasnt allowing us to use styles from the config. Now we can standardise some of these styles by adding them to the config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  extends: {
-    colors: {
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-    },
-  },
   theme: {
     screens: {
       sm: "480px",
@@ -21,6 +9,7 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
+    colors: colors,
     fontFamily: {
       sans: ["Graphik", "sans-serif"],
       serif: ["Merriweather", "serif"],
