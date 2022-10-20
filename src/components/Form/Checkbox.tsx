0a31bf1b0be866f083bb6c02ca2a0e75c595acca
Mad Neutrino <madneutrino@jezer0x.com>
2022-10-20 23:36:19 +0800

    Display twap options

--- src/components/Form/Checkbox.tsx
+++ src/components/Form/Checkbox.tsx
@@ -1,19 +1,31 @@
+import { Switch } from "@headlessui/react";
 import React from "react";
 
 export default function Checkbox(props: {
   isChecked: boolean;
+  label: string;
   setIsChecked: (checked: boolean) => void;
 }) {
-  const { isChecked, setIsChecked } = props;
+  const { isChecked, label, setIsChecked } = props;
 
   return (
-    <div>
-      <input
-        type="checkbox"
-        className="rounded text-pink-500"
-        checked={isChecked}
-        onClick={() => setIsChecked(!isChecked)}
-      />
-    </div>
+    <Switch.Group>
+      <div className="flex items-center">
+        <Switch.Label className="mr-4">{label}</Switch.Label>
+        <Switch
+          checked={isChecked}
+          onChange={setIsChecked}
+          className={`${
+            isChecked ? "bg-blue-600" : "bg-gray-200"
+          } relative inline-flex h-6 w-11 items-center rounded-full`}
+        >
+          <span
+            className={`${
+              isChecked ? "translate-x-6" : "translate-x-1"
+            } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+          />
+        </Switch>
+      </div>
+    </Switch.Group>
   );
 }
--- src/components/Fund/SwapBox.tsx
+++ src/components/Fund/SwapBox.tsx
@@ -3,10 +3,50 @@ import React from "react";
 import { useState } from "react";
 import { Token } from "../../config/tokens";
 import Button from "../Form/Button";
+import Checkbox from "../Form/Checkbox";
 import { Input } from "../Form/Input";
+import Selector from "../Form/Selector";
 import Slider from "../Form/Slider";
 import TokenSelector from "../Form/TokenSelector";
 
+const twapIntervals = ["Min", "Hour", "Day"];
+
+function TwapOptions() {
+  const [batchSize, setBatchSize] = useState(0.25);
+  const [interval, setInterval] = useState(60 * 60); // 1 hour
+
+  return (
+    <div>
+      <div className="mt-4 space-y-3">
+        <Input
+          type="number"
+          name={t`Batch Size`}
+          id="batchSize"
+          value={batchSize * 100}
+          placeholder={t`Batch Size`}
+          onChange={(e) => setBatchSize(parseFloat(e.target.value) / 100)}
+          required
+        />
+        %
+        <Selector
+          items={twapIntervals.map((i) => ({ name: i }))}
+          selectedItem={{ name: twapIntervals[0] }}
+          setSelectedItem={(item) => console.log(item)}
+        />
+        <Input
+          type="number"
+          name={t`Interval`}
+          id="interval"
+          value={interval / 60}
+          placeholder={t`Interval`}
+          onChange={(e) => setInterval(parseFloat(e.target.value) * 60)} // need to adjust based on selector
+          required
+        />
+        <Trans>Estimated Completion: {interval / batchSize / 60} minutes</Trans>
+      </div>
+    </div>
+  );
+}
 export default function SwapBox(props: {
   tokens: Token[];
   amountAvailable: number;
@@ -15,6 +55,7 @@ export default function SwapBox(props: {
   const [fromToken, setFromToken] = useState(tokens[0]);
   const [toToken, setToToken] = useState(tokens[0]);
   const [amountToSwap, setAmountToSwap] = useState(10);
+  const [useTwap, setUseTwap] = useState(false);
 
   return (
     <div>
@@ -51,6 +92,12 @@ export default function SwapBox(props: {
           setAmountToSwap(Math.round((val / 100) * props.amountAvailable))
         }
       />
+      <Checkbox
+        isChecked={useTwap}
+        label={"Enable TWAP"}
+        setIsChecked={setUseTwap}
+      />
+      {useTwap && <TwapOptions />}
       <div className="flex justify-center mt-10">
         <Button type="submit" label={t`Confirm`} />
       </div>
